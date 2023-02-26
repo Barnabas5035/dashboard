a7e18cb98b16f7e9dbababf9ec14431503a47daa
Barnabas5035 <110441604+Barnabas5035@users.noreply.github.com>
2023-02-26 08:23:33 -0800

    added some function

--- src/component/ErrorPage.js
+++ src/component/ErrorPage.js
@@ -2,7 +2,13 @@ import React from 'react'
 import MasterPage from './MainPage/MasterPage'
 
 const ErrorPage = () => {
-  return <MasterPage>ErrorPage</MasterPage>
+  return (
+    <MasterPage>
+      <h4 className='display-9 text-center mt-4'>
+        No Error click on the login buttion
+      </h4>
+    </MasterPage>
+  )
 }
 
 export default ErrorPage
